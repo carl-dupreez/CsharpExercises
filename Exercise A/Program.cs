c5ff131f16960bf12655af19719f6316db58f2d7
Carl du Preez <carlsecure2022@gmail.com>
2023-10-18 08:48:09 +0000

    Completed Exercise A

--- Exercise A/Program.cs
+++ Exercise A/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double salary = 45000;
+double tax = 0;
+
+if(salary <= 8350)
+{
+    tax = salary * 0.10;
+}
+else if(salary> 8350 && salary <= 33950)
+{
+    tax = salary * 0.15;
+}
+else{
+    tax = salary * 0.25;
+}
+
+Console.WriteLine(tax);
