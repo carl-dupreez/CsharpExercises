73bec9d08e6963ea136ae2767355a78e5ecbdf02
Carl du Preez <carlsecure2022@gmail.com>
2023-10-18 08:56:57 +0000

    Completed Exercise B

---
+++ Exercise B/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] strs = {"3.45", "3.87", "87.98", "56.7"};
+
+int strsCount = strs.Count();
+
+double sum = 0;
+
+for(int a = 0; a <= strsCount - 1; a++)
+{
+    sum = sum + Convert.ToDouble(strs[a]);
+}
+
+Console.WriteLine(sum);
